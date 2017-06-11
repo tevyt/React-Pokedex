57043d686477c376d9214212375ebebb8c7f7da7
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-06-11 15:57:18 -0500

    Adds pokemon names to list items

--- src/pokedex/components/pokedex-entry/index.js
+++ src/pokedex/components/pokedex-entry/index.js
@@ -1,7 +1,20 @@
 import React from 'react'; //eslint-disable-line no-unused-vars
 
-export default ({ image }) => {
+import { padNumber } from '../../../utilities';
+
+export default ({ image, name, pokedexNumber }) => {
   return <div className='pokemon-list__item'>
           <img className='pokemon-list__item__image' src={image} />
+          <div className='pokemon-list__item__description'>
+            <span className='pokemon-list__item__description__pokedex-number'>
+            #{padNumber(pokedexNumber, 3)}
+            </span>
+            <span className='pokemon-list__item__description__pokemon-name'>
+              {name.split('-')[0]} {/*Messed up when scraping data from pokemon api
+                                      Some pokemon names have forms e.g Deoxys-Normal
+                                      Drop the '-normal part
+                                      TODO Fix in Database*/}
+            </span>
+          </div>
         </div>;
 };
